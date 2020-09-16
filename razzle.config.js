371c9b2ec93b2d9498ae5a01647c2df41d71ec60
uuz <ncysatnaf@gmail.com>
2020-09-16 21:03:50 +0800

    update build

--- razzle.config.js
+++ razzle.config.js
@@ -3,8 +3,7 @@
 const razzleHeroku = require("razzle-heroku");
 
 module.exports = {
-  plugins: [
-    {
+  plugins: [{
       name: "typescript",
       options: {
         forkTsChecker: {
@@ -14,26 +13,33 @@ module.exports = {
     },
     "scss",
   ],
-  modify: (config, { target, dev }, webpack) => {
-    config = razzleHeroku(config, { target, dev }, webpack);
+  modify: (config, {
+    target,
+    dev
+  }, webpack) => {
+    config = razzleHeroku(config, {
+      target,
+      dev
+    }, webpack);
     if (target == "web") {
-      config.node = { fs: "empty", module: "empty" };
+      config.node = {
+        fs: "empty",
+        module: "empty"
+      };
     }
     if (process.env.ANALYZER) {
       const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
       config.plugins.push(new BundleAnalyzerPlugin());
     }
     if (!dev && target === "web") {
-      config.output.filename = dev ? "static/js/[name].js" : "static/js/[name].[hash:8].js";
-
-      config.entry.vendor = [require.resolve("react"), require.resolve("react-dom"), require.resolve("iotex-antenna")];
-
-      config.optimization = {
+      config.output.filename = "static/js/[name].[hash:8].js";
+      config.entry.vendor = ["react", "react-dom", "iotex-antenna", "mobx", "axios"];
+      Object.assign(config.optimization, {
         splitChunks: {
           chunks: "all",
           name: false,
         },
-      };
+      });
     }
 
     return config;
