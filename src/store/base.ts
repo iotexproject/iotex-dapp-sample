24eb2a4bd61fd192cc55a0fddf5791cee49322af
uuz <ncysatnaf@gmail.com>
2021-06-12 18:13:31 +0800

    cleanup code

--- src/store/base.ts
+++
@@ -1,7 +0,0 @@
-import { makeAutoObservable } from 'mobx';
-
-export class BaseStore {
-  constructor() {
-    makeAutoObservable(this);
-  }
-}
--- src/store/root.ts
+++ src/store/root.ts
@@ -1,10 +1,8 @@
-import { BaseStore } from './base';
 import { LangStore } from './lang';
 import { GodStore } from './god';
 import { TokenStore } from './token';
 
 export class RootStore {
-  base = new BaseStore();
   lang = new LangStore();
   god = new GodStore(this);
   token = new TokenStore(this);
