f340141a43a2acc0ee59b37c2984f76597dd118c
zongzheng <304389312@qq.com>
2021-07-05 17:22:23 +0800

    add wallet address and add the io address when select the iotex network

--- src/components/WalletInfo/index.tsx
+++ src/components/WalletInfo/index.tsx
@@ -9,12 +9,15 @@ import { Button, Text, Flex, Center, Link, Tooltip } from '@chakra-ui/react';
 import { ExternalLinkIcon, CopyIcon } from '@chakra-ui/icons';
 import * as clipboard from 'clipboard-polyfill/text';
 import { helper } from '@/lib/helper';
+import { IotexMainnetConfig } from '../../config/IotexMainnetConfig'
+import { from } from "@iotexproject/iotex-address-ts"
 
 export const WalletInfo = observer(() => {
   const { god, lang } = useStore();
 
   const store = useLocalStore(() => ({
     isTipOpen: false,
+    isIOTipOpen: false,
     get visible() {
       return god.currentNetwork.walletInfo.visible;
     },
@@ -31,30 +34,24 @@ export const WalletInfo = observer(() => {
     },
     toggleTipOpen(val: boolean) {
       this.isTipOpen = val;
+    },
+    toggleIOTipOpen(val: boolean) {
+      this.isTipOpen = val;
     }
   }));
-  console.log(`${god.currentChain.explorerURL}/adress/${god.currentChain.explorerName}`);
   return (
-    <Modal isOpen={store.visible} onClose={store.close} isCentered>
+    <Modal isOpen={store.visible} size="lg" onClose={store.close} isCentered>
       <ModalOverlay />
       <ModalContent padding="10">
         <Center>
-          <Text>{god.currentNetwork.account}</Text>
-        </Center>
-        <Center mt="16px">
-          <Link target="_blank" display="flex" alignItems="center" color="blue.400" href={`${god.currentChain.explorerURL}/adress/${god.currentChain.explorerName}`}>
-            View On BscScan
-            <ExternalLinkIcon ml="4px" w="1.4rem" h="1.4rem" />
-          </Link>
           <Tooltip label="Copied" placement="bottom" isOpen={store.isTipOpen}>
             <Text
               display="flex"
               alignItems="center"
-              ml="4rem"
               color="blue.400"
               cursor="pointer"
               onClick={async () => {
-                const [error] = await helper.promise.runAsync(clipboard.writeText(god.currentNetwork.account))
+                const [error] = await helper.promise.runAsync(clipboard.writeText(god.currentNetwork.account));
                 if (!error) {
                   store.toggleTipOpen(true);
                   setTimeout(() => {
@@ -63,10 +60,40 @@ export const WalletInfo = observer(() => {
                 }
               }}
             >
-              Copy Address
+              {god.currentNetwork?.account}
               <CopyIcon ml="4px" w="1.4rem" h="1.4rem" />
             </Text>
           </Tooltip>
+        </Center>
+        {  god.isIotxNetork && 
+          <Center mt="8px">
+          <Tooltip label="Copied" placement="bottom" isOpen={store.isIOTipOpen}>
+            <Text
+              display="flex"
+              alignItems="center"
+              color="blue.400"
+              cursor="pointer"
+              onClick={async () => {
+                const [error] = await helper.promise.runAsync(clipboard.writeText(from(god.currentNetwork.account)?.string()));
+                if (!error) {
+                  store.toggleIOTipOpen(true);
+                  setTimeout(() => {
+                    store.toggleIOTipOpen(false);
+                  }, 500);
+                }
+              }}
+            >
+              {god.currentNetwork?.account && from(god.currentNetwork.account)?.string()}
+              <CopyIcon ml="4px" w="1.4rem" h="1.4rem" />
+            </Text>
+          </Tooltip>
+        </Center>
+        }
+        <Center mt="16px">
+          <Link target="_blank" display="flex" alignItems="center" color="blue.400" href={`${god.currentChain.explorerURL}/adress/${god.currentChain.explorerName}`}>
+            View On BscScan
+            <ExternalLinkIcon ml="4px" w="1.4rem" h="1.4rem" />
+          </Link>
         </Center>
         <Button mt="24px" onClick={store.logout} size="md">
           <Text>Logout</Text>
