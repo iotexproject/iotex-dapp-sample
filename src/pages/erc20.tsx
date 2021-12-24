3d77911ba0fe9c661c6d1f201708ac1ddf70934f
wanyulong <15225182601@163.com>
2021-12-24 19:10:44 +0800

    update zh_CN; change method of get Allowance

--- src/pages/erc20.tsx
+++ src/pages/erc20.tsx
@@ -5,7 +5,17 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Center, Text } from '@chakra-ui/layout';
 import toast from 'react-hot-toast';
 
-import { Container, FormControl, Input, Button, Image, InputGroup, InputRightElement, Flex, Box } from '@chakra-ui/react';
+import {
+  Container,
+  FormControl,
+  Input,
+  Button,
+  Image,
+  InputGroup,
+  InputRightElement,
+  Flex,
+  Box
+} from '@chakra-ui/react';
 import { useStore } from '../store/index';
 import { StringState, BooleanState } from '../store/standard/base';
 import { TokenListModal } from '../components/TokenListModal/index';
@@ -41,18 +51,26 @@ const ERC20 = observer(() => {
     },
     onSelectToken(token: TokenState) {
       store.curToken = token;
+      store.allowance = new BigNumberState({});
     },
     allowance: new BigNumberState({}),
     loadAllowance() {
       // TODO: check params
-      god.currentNetwork.multicall([store.curToken.preMulticall({ method: 'allowance', params: [god.currentNetwork.account, store.receiverAdderss.value], handler: store.allowance })]);
+      if (store.receiverAdderss.value && store.curToken && store.amount) {
+        god.currentNetwork.multicall([store.curToken.preMulticall(
+          {
+            method: 'allowance',
+            params: [god.currentNetwork.account, store.receiverAdderss.value],
+            handler: store.allowance
+          })]);
+      } else {
+        store.allowance = new BigNumberState({});
+      }
     },
-
     async onSubmit() {
       if (store.state.connectWallet) {
         return god.setShowConnecter(true);
       }
-
       const res = await store.curToken.transfer.call({ params: [store.receiverAdderss.value, store.amount.value.toFixed(0, 1)] });
 
       const receipt = await res.wait();
@@ -82,41 +100,52 @@ const ERC20 = observer(() => {
     });
   }, []);
   return (
-    <Container maxW="md">
+    <Container maxW='md'>
       <form>
         <FormControl mt={20}>
-          <Box border="1px solid" borderRadius="md" borderColor="inherit">
-            <Flex justify="space-between" p={2}>
-              <Text fontSize="sm">{lang.t('token.amount')}</Text>
-              <Text fontSize="sm">{store.curToken ? `Balance ${store.curToken.balance.format} ` : '...'}</Text>
+          <Box border='1px solid' borderRadius='md' borderColor='inherit'>
+            <Flex justify='space-between' p={2}>
+              <Text fontSize='sm'>{lang.t('token.amount')}</Text>
+              <Text fontSize='sm'>{store.curToken ? `Balance ${store.curToken.balance.format} ` : '...'}</Text>
             </Flex>
             <InputGroup>
-              <Input border="none" placeholder="0.0" type="number" value={store.amount.format} onChange={(e) => store.amount.setFormat(e.target.value)} />
-              <InputRightElement onClick={store.openTokenList} width="4rem" cursor="pointer" flexDir="column">
-                <Flex alignItems="center" pr={2} w="100%">
-                  <Image borderRadius="full" boxSize="24px" src={store.curToken?.logoURI} fallbackSrc="/images/token.svg" />
+              <Input border='none' placeholder='0.0' type='number' value={store.amount.format} onChange={(e) => {
+                store.amount.setFormat(e.target.value);
+              }} onBlur={store.loadAllowance} />
+              <InputRightElement onClick={store.openTokenList} width='4rem' cursor='pointer' flexDir='column'>
+                <Flex alignItems='center' pr={2} w='100%'>
+                  <Image borderRadius='full' boxSize='24px' src={store.curToken?.logoURI}
+                         fallbackSrc='/images/token.svg' />
                   <Icon as={ChevronDownIcon} ml={1} />
                 </Flex>
               </InputRightElement>
             </InputGroup>
           </Box>
 
-          <Box border="1px solid" borderRadius="md" borderColor="inherit" mt={4}>
-            <Flex justify="space-between" p={2}>
-              <Text fontSize="sm">{lang.t('receiver.address')}</Text>
-              <Text fontSize="sm">{store.curToken ? `Allowance ${store.curToken.allowanceForRouter.format} ` : '...'}</Text>
+          <Box border='1px solid' borderRadius='md' borderColor='inherit' mt={4}>
+            <Flex justify='space-between' p={2}>
+              <Text fontSize='sm'>{lang.t('receiver.address')}</Text>
+              <Text fontSize='sm'>{store.curToken ? `Allowance ${store.allowance.format} ` : '...'}</Text>
             </Flex>
             <InputGroup>
-              <Input border="none" placeholder={god.currentNetwork.info.token.tokenExample} value={store.receiverAdderss.value} onChange={(e) => store.receiverAdderss.setValue(e.target.value)} />
+              <Input border='none' placeholder={god.currentNetwork.info.token.tokenExample}
+                     value={store.receiverAdderss.value}
+                     onChange={(e) => {
+                       store.receiverAdderss.setValue(e.target.value);
+                     }}
+                     onBlur={store.loadAllowance}
+              />
             </InputGroup>
           </Box>
 
-          <Flex justify="space-around" p={2}>
-            <Button type="button" mt="4" disabled={!store.state.valid || store.loading?.value} onClick={store.onSubmit} isLoading={store.loading?.value}>
+          <Flex justify='space-around' p={2}>
+            <Button type='button' mt='4' disabled={!store.state.valid || store.loading?.value} onClick={store.onSubmit}
+                    isLoading={store.loading?.value}>
               {store.state.msg}
             </Button>
-            {store.state.valid && (
-              <Button type="button" mt="4" disabled={!store.state.valid || store.loading?.value} onClick={store.onApprove} isLoading={store.loading?.value}>
+            {store.state.valid && god.isConnect && (
+              <Button type='button' mt='4' disabled={!store.state.valid || store.loading?.value}
+                      onClick={store.onApprove} isLoading={store.loading?.value}>
                 {/*{lang.t('approve')}*/}
                 {store.state.msgApprove}
               </Button>
@@ -124,7 +153,8 @@ const ERC20 = observer(() => {
           </Flex>
         </FormControl>
       </form>
-      <TokenListModal isOpen={store.isOpenTokenList.value} onClose={() => store.isOpenTokenList.setValue(false)} onSelect={store.onSelectToken} />
+      <TokenListModal isOpen={store.isOpenTokenList.value} onClose={() => store.isOpenTokenList.setValue(false)}
+                      onSelect={store.onSelectToken} />
     </Container>
   );
 });
