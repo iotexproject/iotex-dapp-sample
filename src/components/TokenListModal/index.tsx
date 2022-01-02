b7d12a478722cd175daca5c9bdd5874dd080ca86
ccuoo <527643957@qq.com>
2022-01-02 09:52:25 +0800

    temporary save

--- src/components/TokenListModal/index.tsx
+++ src/components/TokenListModal/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { observer, useLocalObservable } from 'mobx-react-lite';
-import { Modal, ModalContent, ModalHeader, ModalOverlay, ModalCloseButton, ListItem, Image, Button, Box, Input, Img, Flex, useColorModeValue } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalCloseButton,
+  ListItem,
+  Image,
+  Button,
+  Box,
+  Input,
+  Grid,
+  GridItem,
+  Tooltip,
+  useColorModeValue
+} from '@chakra-ui/react';
+import { ChevronLeftIcon, QuestionIcon } from '@chakra-ui/icons';
 import { useStore } from '../../store/index';
 import TokenState from '../../store/lib/TokenState';
 import { StringState } from '../../store/standard/base';
 import { Text } from '@chakra-ui/layout';
 import VirtualList from 'react-tiny-virtual-list';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { reaction } from 'mobx';
 
 interface PropsType {
@@ -18,6 +34,7 @@ interface PropsType {
 
 export const TokenListModal = observer((props: PropsType) => {
   const { god, token, lang } = useStore();
+  const [isDrillDown, setDrill] = useState<boolean>(false);
   const store = useLocalObservable(() => ({
     keyword: new StringState(),
     newToken: null as TokenState,
@@ -41,6 +58,7 @@ export const TokenListModal = observer((props: PropsType) => {
       store.keyword.setValue('');
       store.newToken = null;
       props.onClose();
+      setDrill(false);
     },
     onSelect(item: TokenState) {
       props.onSelect(item);
@@ -64,8 +82,15 @@ export const TokenListModal = observer((props: PropsType) => {
             [
               newToken.preMulticall({ method: 'symbol', handler: (v: any) => (newToken.symbol = v.toString()) }),
               newToken.preMulticall({ method: 'name', handler: (v: any) => (newToken.name = v.toString()) }),
-              newToken.preMulticall({ method: 'decimals', handler: (v: any) => (newToken.decimals = Number(v.toString())) }),
-              god.currentNetwork.account ? newToken.preMulticall({ method: 'balanceOf', params: [god.currentNetwork.account], handler: newToken._balance }) : null
+              newToken.preMulticall({
+                method: 'decimals',
+                handler: (v: any) => (newToken.decimals = Number(v.toString()))
+              }),
+              god.currentNetwork.account ? newToken.preMulticall({
+                method: 'balanceOf',
+                params: [god.currentNetwork.account],
+                handler: newToken._balance
+              }) : null
             ].filter((i) => !!i)
           );
           store.newToken = newToken;
@@ -75,58 +100,76 @@ export const TokenListModal = observer((props: PropsType) => {
       }
     );
   }, []);
-  console.log(props.blackList);
-
   const tokenNameColor = useColorModeValue('gray.400', 'dark.300');
-
+  const temCom = [];
   return (
     <Modal isOpen={props.isOpen} onClose={store.onClose} closeOnEsc closeOnOverlayClick>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{lang.t('select.token')}</ModalHeader>
+        <ModalHeader>{isDrillDown ? <Grid gap={4}>
+          <GridItem colSpan={2}><ChevronLeftIcon cursor='pointer' w={8} h={8}
+                                                 onClick={() => setDrill(false)} /></GridItem>
+          <GridItem colStart={3} colEnd={6}>{lang.t('manage')}</GridItem>
+        </Grid> : lang.t('select.token')}</ModalHeader>
         <ModalCloseButton />
-        <Box px={4} pb={2}>
-          <Input placeholder={lang.t('select.token.placeholder')} value={store.keyword.value} onChange={(e) => store.keyword.setValue(e.target.value)} />
-        </Box>
+        {!isDrillDown && <Box px={4} pb={2}>
+          <Input placeholder={lang.t('select.token.placeholder')} value={store.keyword.value}
+                 onChange={(e) => store.keyword.setValue(e.target.value)} />
+        </Box>}
+        {
+          !isDrillDown &&
+          <Box ml={4}><Text fontWeight='500'>Common bases <Tooltip
+            label='These tokens are commonly paired with other tokens.'>
+            <QuestionIcon cursor='help' />
+          </Tooltip></Text> <Grid gap={4}
+                                  templateColumns='repeat(4, 1fr)'> {store.tokens.length && store.tokens.length > 10 && store.tokens.slice(0, 10).map((obj) =>
+            <GridItem w='100%' onClick={() => store.onSelect(obj)}>
+              <Button variant={'ghost'} w='100%' display='flex' alignItems='center'>
+                <Image borderRadius='full' boxSize='24px' src={obj.logoURI} mr='4'
+                       fallbackSrc='/images/token.svg' />
+                <Text fontWeight='500'>{obj.symbol}</Text>
+              </Button>
+            </GridItem>
+          )}
+          </Grid>
+          </Box>
+        }
         <Box ml={4}>
           <VirtualList
-            width="100%"
-            height={600}
+            width='100%'
+            height={500}
             itemSize={50}
             style={{ paddingRight: '15px' }}
             itemCount={store.tokens.length}
             renderItem={({ index, style }) => {
               const i = store.tokens[index];
-
               return (
-                <ListItem my={1} key={index} {...style} cursor="pointer" display="flex" alignItems="center" justifyContent="space-between">
+                <ListItem my={1} key={index} {...style} cursor='pointer' display='flex' alignItems='center'
+                          justifyContent='space-between'>
                   <Button
-                    p="2"
+                    p='2'
                     disabled={props.blackList?.includes(i.address)}
                     variant={'ghost'}
                     display={'flex'}
-                    w="full"
+                    w='full'
                     justifyContent={'space-between'}
                     textAlign={'left'}
                     onClick={() => store.onSelect(i)}
                   >
-                    <Box display="flex" alignItems="center">
-                      <Image borderRadius="full" boxSize="24px" src={i.logoURI} mr="4" fallbackSrc="/images/token.svg" />
+                    <Box display='flex' alignItems='center'>
+                      <Image borderRadius='full' boxSize='24px' src={i.logoURI} mr='4'
+                             fallbackSrc='/images/token.svg' />
                       <Box>
-                        <Text fontWeight="500">{i.symbol}</Text>
-                        <Text fontSize="xs" color={tokenNameColor}>
-                          {i.name}
-                          {i.saved && ' • Added by user'}
-                        </Text>
+                        <Text fontWeight='500'>{i.symbol}</Text>
                       </Box>
                     </Box>
-                    <Box display="flex" alignItems="center">
+                    <Box display='flex' alignItems='center'>
                       <Text>{i.balance.format}</Text>
                       {i.isNew && (
                         <Button
-                          ml="4"
-                          px="4"
-                          size="sm"
+                          ml='4'
+                          px='4'
+                          size='sm'
                           onClick={(e) => {
                             e.stopPropagation();
                             store.addToken(i);
@@ -141,6 +184,11 @@ export const TokenListModal = observer((props: PropsType) => {
               );
             }}
           />
+          {!isDrillDown && <Button
+            width='90%'
+            margin='0 auto 10px 10px'
+            onClick={() => setDrill(true)}
+          >{lang.t('manage.token.list')}</Button>}
         </Box>
       </ModalContent>
     </Modal>
