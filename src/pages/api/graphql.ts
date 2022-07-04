a6899a25bbda9c2d97981f561b5713fdd7afc54e
uuz <ncysatnaf@gmail.com>
2022-07-04 15:57:26 +0800

    fix build

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -8,7 +8,7 @@ const server = createServer<{
 }>({
   schema,
   context: ({ req, res }) => {
-    return { smartGraph, dataloader: smartGraph.dataloader, req, res };
+    return { smartGraph, req, res };
   },
 });
 
