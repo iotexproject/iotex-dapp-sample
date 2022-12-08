cfafc973c15efa285ba358a568615b789fe2771b
zongzheng123 <304389312@qq.com>
2022-12-08 18:16:31 +0800

    upgrade trpc

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,33 +1,13 @@
-/**
- * This file contains the root router of your tRPC-backend
- */
-import superjson from 'superjson';
-import { createRouter } from '../createRouter';
-import { templateRouter } from './template';
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
 
-/**
- * Create your application's root router
- * If you want to use SSG, you need export this
- * @link https://trpc.io/docs/ssg
- * @link https://trpc.io/docs/router
- */
-export const appRouter = createRouter()
-  /**
-   * Add data transformers
-   * @link https://trpc.io/docs/data-transformers
-   */
-  .transformer(superjson)
-  /**
-   * Optionally do custom error (type safe!) formatting
-   * @link https://trpc.io/docs/error-formatting
-   */
-  // .formatError(({ shape, error }) => { })
-  // .merge('post.', postRouter);
-  .query('healthz', {
-    async resolve() {
+export const appRouter = router({
+  healthz: procedure
+  .query(async () => {
       return 'ok';
     }
-  })
-  .merge('dev.', templateRouter);
+  )
+});
 
+// export type definition of API
 export type AppRouter = typeof appRouter;
--- src/server/routers/template.ts
+++ src/server/routers/template.ts
@@ -1,7 +1,17 @@
-import { createRouter } from 'server/createRouter';
 
-export const templateRouter = createRouter().query('health', {
-  async resolve({ ctx }) {
-    return 'ok';
-  }
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
+export const templateRouter = router({
+  whoami: procedure.
+  input(z.object({
+    name: z.string()
+  })).
+  output(z.object({
+    name: z.string()
+  }))
+  .query(async ({input}) => {
+    const {name} = input
+    return name
+    }
+  )
 });
